1ab0fa871e97aecc41e516911c17e4d6b95002b8
samwel okoth <samwelokoth8@gmail.com>
2024-11-21 06:57:20 -0500

    rotating 2d matrix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,32 +1,21 @@
-#!/usr/bin/python3
-""" pascal triangle
-"""
+def generate_pascals_triangle(rows):
+    triangle = []
+    
+    for i in range(rows):
+        row = [1]
+        if i > 0:
+            prev_row = triangle[-1]
+            for j in range(len(prev_row) - 1):
+                row.append(prev_row[j] + prev_row[j+1])
+            row.append(1)
+        triangle.append(row)
+    
+    return triangle
 
+def print_pascals_triangle(triangle):
+    for row in triangle:
+        print(row)
 
-def pascal_triangle(n):
-    """ returns pascal triangle
-    """
-    if n <= 0:
-        return []
-
-    pasTran = []
-
-    for i in range(n):
-        # first element
-        my_List = [1]
-        if i == 0:
-            pasTran.append(my_List)
-            continue
-
-        k = i - 1
-        for j in range(len(pasTran[k])):
-            if j + 1 == len(pasTran[k]):
-                # last element
-                my_List.append(1)
-                break
-            # Add two previous values to get current next value
-            nextVal = pasTran[k][j] + pasTran[k][j + 1]
-            my_List.append(nextVal)
-        pasTran.append(my_List)
-
-    return pasTran
+num_rows = int(input("Enter the number of rows for Pascal's Triangle: "))
+triangle = generate_pascals_triangle(num_rows)
+print_pascals_triangle(triangle)
